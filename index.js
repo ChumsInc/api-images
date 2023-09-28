38669284d8d29e5ec85f6746fe27b7dc29550949
Steve Montgomery <steve@chums.com>
2023-09-28 16:09:39 -0600

    chore: cleanup unused modules

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.set('trust proxy', 'loopback');
 app.use(compression());
 app.use(helmet());
 app.set('json spaces', 2);
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
