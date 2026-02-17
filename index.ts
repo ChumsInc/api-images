7e362629ad60bbdf66d30cbab24cf4575a445b4f
Steve Montgomery <steve@chums.com>
2026-02-17 13:12:19 -0700

    chore: - add eslint - code cleanup

---
+++ index.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import Debug from 'debug';
+import express from 'express';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import http from 'node:http';
+import {router} from './lib/index.js';
+
+if (!process.env.PORT) {
+    console.log('*** error loading .env - PORT is not set',);
+    process.exit(1);
+}
+const debug = Debug('chums:index');
+
+
+const app = express();
+app.set('trust proxy', 'loopback');
+app.use(helmet());
+app.set('json spaces', 2);
+// app.set('view engine', 'pug');
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(router);
+
+const {PORT, NODE_ENV} = process.env;
+const server = http.createServer(app);
+
+
+server.listen(PORT);
+debug(`server started on port: ${PORT}; mode: ${NODE_ENV}`);
